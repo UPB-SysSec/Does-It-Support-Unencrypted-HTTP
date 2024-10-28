b438b6c3446b6173828e21642c4c169e87342069
JonSnowWhite <nniklas@mail.upb.de>
2024-10-28 08:54:12 +0100

    Added docstrings

--- analyze.py
+++ analyze.py
@@ -15,6 +15,9 @@ BUFFER_SIZE = 4096
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0"
 
 def main():
+    """
+    Main method. Initializes parser and starts Analyzer.
+    """
 
     # prepare argument parser
     parser = argparse.ArgumentParser(description='Analyzes servers for unencrypted HTTP support.',
@@ -34,7 +37,21 @@ def main():
     Analyzer(args.hostname, args.path, args.ip, args.port, args.debug, args.redirect_depth, args.timeout).analyze()
 
 class Analyzer:
+    """
+    Analyzes a website for unencrypted HTTP support.
+    """
+
     def __init__(self, hostname: str, path: str, ip: str = None, port: int =  80, debug: bool = False, redirect_depth: int = 1, timeout: int = 5):
+        """
+        Initializes Analyzer.
+        :param hostname: The hostname of the server to analyze. Used in the Host header of HTTP requests. (required)
+        :param path: The path to request from the server. (required)
+        :param ip: The IP of the server to analyze. If not provided, the hostname is resolved. (optional)
+        :param port: The port of the server to analyze. (default: 80)
+        :param debug: Enables debug statements if True. (default: False)
+        :param redirect_depth: The maximum depth of HTTP redirects to follow. (default: 1)
+        :param timeout: The timeout for socket operations in seconds. (default: 5)
+        """
         self.hostname = hostname
         self.path = path
         self.ip = ip
@@ -44,16 +61,23 @@ class Analyzer:
         self.timeout = timeout
 
     def analyze(self):
+        """
+        Analyzes the server for unencrypted HTTP support.
+
+        The following steps are performed:
+        1. Resolve hostname if necessary
+        2. Check TCP connectivity of server
+        3. Analyze HTTP/0.9 support
+        4. Analyze HTTP/1.0 support
+        5. Analyze HTTP/1.1 support
+        6. Analyze HTTP/2.0 support with prior knowledge
+        7. Analyze HTTP/2.0 support with upgrade mechanism
+        8. Print results
+        """
         # resolve hostname if necessary
         if self.ip is None:
             self._debug("No IP provided, attempting to resolve hostname")
-            try:
-                self.ip = socket.gethostbyname(self.hostname)
-                self._debug("Resolved hostname " + self.hostname + " to IP: " + self.ip)
-            except Exception as e:
-                self._debug("Failed to resolve hostname " + self.hostname + " with Exception: " + str(e))
-                print("No IP provided and cannot resolve hostname for " + self.hostname + ". Provide reachable IP address or resolvable hostname.")
-                exit(255)
+            self.resolve_hostname()
         else:
             self._debug("Using given IP " + self.ip + " on " + self.hostname)
 
@@ -109,6 +133,12 @@ class Analyzer:
         print("HTTP/2 upgrade: " + ret_20_upgrade)
 
     def analyze_http09(self) -> str:
+        """
+        Analyzes the server for HTTP/0.9 support.
+        :return: SUCCESS if HTTP/0.9 is supported, FAILURE otherwise.
+
+        HTTP/0.9 is supported if the server responds with an HTML response.
+        """
         # open and connect socket
         sock = self.open_socket()
         if self.connect_socket(sock) != SUCCESS:
@@ -137,12 +167,34 @@ class Analyzer:
 
 
     def analyze_http10(self, recursion: int) -> str:
+        """
+        Analyzes the server for HTTP/1.0 support.
+        :param recursion: The maximum depth of redirects to follow.
+        :return: SUCCESS if HTTP/1.0 is supported, FAILURE otherwise.
+
+        HTTP/1.0 is supported if the server responds with a 200 status code.
+        """
         return self.analyze_http1x("HTTP/1.0", recursion)
 
     def analyze_http11(self, recursion: int) -> str:
+        """
+        Analyzes the server for HTTP/1.1 support.
+        :param recursion: The maximum depth of redirects to follow.
+        :return: SUCCESS if HTTP/1.1 is supported, FAILURE otherwise.
+
+        HTTP/1.1 is supported if the server responds with a 200 status code.
+        """
         return self.analyze_http1x("HTTP/1.1", recursion)
 
     def analyze_http1x(self, version: str, recursion: int):
+        """
+        Analyzes the server for HTTP/1.x support.
+        :param version: The HTTP version to analyze. Provide the complete string, e.g. "HTTP/1.0".
+        :param recursion: The maximum depth of redirects to follow.
+        :return: SUCCESS if HTTP/1.x is supported, FAILURE otherwise.
+
+        HTTP/1.x is supported if the server responds with a 200 status code.
+        """
         if recursion == -1:
             return "Max redirect"
         # open and connect socket
@@ -176,13 +228,39 @@ class Analyzer:
             return FAILURE
 
     def analyze_http2_prior_knowledge(self, recursion: int) -> str:
+        """
+        Analyzes the server for HTTP/2 support with prior knowledge.
+        :param recursion: The maximum depth of redirects to follow.
+        :return: SUCCESS if HTTP/2 is supported, FAILURE otherwise.
+
+        HTTP/2 with prior knowledge is supported if the server immediately responds with a 200 status code in an HTTP/2
+        message.
+        """
         return self.analyze_http2_core(recursion, True)
 
 
     def analyze_http2_upgrade(self, recursion: int) -> str:
+        """
+        Analyzes the server for HTTP/2 support with upgrade mechanism.
+        :param recursion: The maximum depth of redirects to follow.
+        :return: SUCCESS if HTTP/2 is supported, FAILURE otherwise.
+
+        HTTP/2 with upgrade mechanism is supported if the server responds with a 101 status code in an HTTP/1.1 message
+        and a 200 status code in an HTTP/2 message.
+        """
         return self.analyze_http2_core(recursion, False)
 
     def analyze_http2_core(self, recursion: int, prior_knowledge: bool) -> str:
+        """
+        Analyzes the server for HTTP/2 support using either prior knowledge or the upgrade mechanism.
+        :param recursion: The maximum depth of redirects to follow.
+        :param prior_knowledge: Whether to use prior knowledge or the upgrade mechanism. True for prior knowledge,
+        False for upgrade.
+        :return: SUCCESS if HTTP/2 is supported, FAILURE otherwise
+
+        This function provides the core HTTP/2 functionality. It performs the upgrade mechanism or prior knowledge and
+        interprets all HTTP/2 responses in a shared loop.
+        """
         if recursion == -1:
             return MAX_REDIRECT
 
@@ -306,7 +384,12 @@ class Analyzer:
 
     def update_redirect(self, headers: dict) -> str:
         """
-        Updates self.hostname and self.path with the new location from the headers.
+        Updates the hostname and path based on a redirect response's headers.
+        :param headers: The headers of the redirect response.
+        :return: SUCCESS if the redirect was successful, FAILURE otherwise.
+
+        The hostname and path are updated based on the Location header of the redirect response. Updates the object
+        variables.
         """
         if "location" not in headers:
             self._debug("No location header in redirect response.")
@@ -338,9 +421,18 @@ class Analyzer:
         return SUCCESS
 
     def create_http_09_request(self) -> bytes:
+        """
+        Creates an HTTP/0.9 request.
+        :return: The encoded HTTP/0.9 request.
+        """
         return b"GET " + self.path.encode("ASCII") + b"\r\n"
 
     def create_http1x_request(self, version: str) -> bytes:
+        """
+        Creates an HTTP/1.x request.
+        :param version: The HTTP version to use. Provide the complete string, e.g. "HTTP/1.0".
+        :return: The encoded HTTP/1.x request.
+        """
         return (
                 b"GET " + self.path.encode("ASCII") + b" " + version.encode("ASCII") + b"\r\n" +
                 b"Host: " + self.hostname.encode("ASCII") + b"\r\n" +
@@ -350,6 +442,11 @@ class Analyzer:
         )
 
     def create_http11_upgrade_request(self, settings_header_value: bytes) -> bytes:
+        """
+        Creates an HTTP/1.1 request to upgrade to HTTP/2. I.e., an HTTP/1.1 request with an Upgrade header.
+        :param settings_header_value: The value of the HTTP2-Settings header.
+        :return: The encoded HTTP/1.1 request.
+        """
         return (
                 b"GET " + self.path.encode("ASCII") + b" HTTP/1.1\r\n" +
                 b"Host: " + self.hostname.encode("ASCII") + b"\r\n" +
@@ -360,34 +457,22 @@ class Analyzer:
                 b"\r\n"
         )
 
-    def parse_http1_response(self, response: str):
-        """
-        Extracts the status code, headers, and HTTP version from an HTTP/1 response.
-
-        :param response: The encoded HTTP response
-        :return: The status code, headers, and HTTP version
-        """
-        response_lines = response.split('\r\n')
-        status = int(response_lines[0].split(' ')[1])
-        http_version = response_lines[0].split(' ')[0].split('/')[1]
-        headers = {}
-        for line in response_lines[1:]:
-            if line == '':
-                break
-
-            key, value = line.split(':', 1)
-            key = key.strip().lower()
-            headers[key] = value.strip().lower()
-
-        return status, headers, http_version
-
     def open_socket(self) -> socket.socket:
+        """
+        Opens a TCP socket to the server.
+        :return: The opened socket.
+        """
         self._debug("Opening TCP socket to " + self.ip + ":" + str(self.port) + "(" + self.hostname + ")")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(self.timeout)
         return sock
 
     def connect_socket(self, sock: socket.socket) -> str:
+        """
+        Connects a previously opened socket to the server.
+        :param sock: The socket to connect.
+        :return: SUCCESS if the connection was successful, FAILURE otherwise.
+        """
         try:
             sock.connect((self.ip, self.port))
         except Exception as e:
@@ -397,6 +482,13 @@ class Analyzer:
         return SUCCESS
 
     def receive_ascii_response(self, sock: socket.socket) -> str:
+        """
+        Reads bytes from the socket and returns them as an ASCII string.
+        :param sock: The socket to read from.
+        :return: The ASCII response as a string.
+
+        If the response is empty, TIMEOUT is returned. If the response cannot be read, FAILURE is returned.
+        """
         try:
             response = sock.recv(BUFFER_SIZE)
         except socket.timeout as e:
@@ -415,7 +507,38 @@ class Analyzer:
             return FAILURE
         return response
 
+    def parse_http1_response(self, response: str) -> (int, dict, str):
+        """
+        Parses an HTTP/1.x response. Extracts status code, headers, and HTTP version.
+        :param response: The response to parse.
+        :return: The status code, headers, and HTTP version as a tuple.
+
+        The response is expected to be a valid HTTP/1.x response.
+        """
+        response_lines = response.split('\r\n')
+        status = int(response_lines[0].split(' ')[1])
+        http_version = response_lines[0].split(' ')[0].split('/')[1]
+        headers = {}
+        for line in response_lines[1:]:
+            if line == '':
+                break
+
+            key, value = line.split(':', 1)
+            key = key.strip().lower()
+            headers[key] = value.strip().lower()
+
+        return status, headers, http_version
+
     def receive_http1x_response(self, sock: socket.socket) -> (int, dict, str):
+        """
+        Receives an HTTP/1.x response from the server. Parses the response and returns the status code, headers, and
+        HTTP version.
+        :param sock: The socket to receive the response from.
+        :return: The status code, headers, and HTTP version as a tuple.
+
+        If the response is empty, TIMEOUT is returned. If the response cannot be parsed, FAILURE is returned. Merges
+        the functionality of receive_ascii_response and parse_http1_response.
+        """
         response = self.receive_ascii_response(sock)
         if response == FAILURE or response == TIMEOUT:
             return response
@@ -427,12 +550,25 @@ class Analyzer:
             return FAILURE
         return status, headers, http_version
 
-    def resolve_hostname(self) -> str:
-        ip = socket.gethostbyname(self.hostname)
-        self._debug("Resolved hostname " + self.hostname + " to IP: " + ip)
-        return ip
+    def resolve_hostname(self):
+        """
+        Resolves the hostname to an IP address. Exits the program if the hostname cannot be resolved. Set the object
+        variable ip to the resolved IP address.
+        """
+        try:
+            self.ip = socket.gethostbyname(self.hostname)
+            self._debug("Resolved hostname " + self.hostname + " to IP: " + self.ip)
+        except Exception as e:
+            self._debug("Failed to resolve hostname " + self.hostname + " with Exception: " + str(e))
+            print(
+                "No IP provided and cannot resolve hostname for " + self.hostname + ". Provide reachable IP address or resolvable hostname.")
+            exit(255)
 
     def analyze_tcp_reachability(self) -> str:
+        """
+        Analyzes the server for TCP reachability. Opens a TCP socket to the server. Returns SUCCESS if the server is
+        reachable, FAILURE otherwise. Returns TIMEOUT if the server is not reachable due to a timeout.
+        """
         # open tcp socket to ip
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(5)
@@ -447,9 +583,14 @@ class Analyzer:
             self._debug("Could not open TCP socket to " + self.ip + ":" + str(self.port) + "(" + self.hostname + ") with exception : " + str(e))
             return FAILURE
 
-    def _debug(self, string: str) -> None:
+    def _debug(self, string: str):
+        """
+        Prints a debug message if debug is enabled.
+        :param string: The debug message to print.
+        """
         if self.debug:
             print("DEBUG:" + string)
 
+# start main method
 if __name__ == '__main__':
     main()
